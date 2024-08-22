9b9ec39b28474e845b9032ce38c21b8f5e3c426f
Vander Moleker <noierdev@proton.me>
2024-08-22 10:18:44 -0400

    update

--- sdk/program/src/account_info.rs
+++ sdk/program/src/account_info.rs
@@ -215,7 +215,7 @@ impl<'a> AccountInfo<'a> {
             owner,
             executable,
             rent_epoch,
-            custom_data
+            custom_data: Rc::new(RefCell::new(data))
         }
     }
 
